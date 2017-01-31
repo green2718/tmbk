b565cc5379a5e55e2d715db7b97feabc38b7ff04
Ghislain Fournier <green@localhost.localdomain>
2017-02-01 00:42:53 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>TMBK: awesome!</title>
+    </head>
+</html>
+<body>
+   <h1>Bienvenue sur <strong class="title">The Most Beautiful Kitten</strong></h1>
+   <p>
+     Le but de ce site est de déterminer, parmi les <strong>chatons</strong>, lequel est le plus mignon.<br/>
+     Nous sommes d'accord que <span class="it">TOUS</span> les <strong>chatons</strong> sont mignons, nous chercherons juste à savoir lequel l'est le plus.
+  </p>
+  <p>
+    Pour cela, votre aide est nécessaire&nbsp;: il faudra cliquer sur le <strong>chaton</strong> le plus mignon des deux. Petit à petit, nous pourrons déterminer lequel est le plus mignon.<br/>
+    Rassurez-vous&nbsp;: si les deux sont trop mignons, il est toujours possible de passer aux suivants en cliquant au milieu.
+  <p>
+  <p>
+    Maintenant, à vous de jouer&nbsp;!
+  </p>
+</body>
