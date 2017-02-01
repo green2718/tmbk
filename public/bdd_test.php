3b5ba77dd050a9568167fca83c1c2d90723c1137
Ghislain FOURNIER <ghislain.fournier@rez-gif.supelec.fr>
2017-02-01 22:47:35 +0100

    Test files for JSON format and DB

---
+++ public/bdd_test.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include '../bdd/conf.php';
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully"; 
+}
+catch(PDOException $e)
+{
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
