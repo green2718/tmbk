75a1c71d870ca07fa153489669da0517b1fa8c97
Ghislain Fournier <ghislain.fournier@rez-gif.supelec.fr>
2017-02-04 15:34:38 +0100

    TODO update

--- public/tmbk.js
+++ public/tmbk.js
@@ -1,12 +1,16 @@
+var reqLock = false;
 function setEventA() {
 	document.getElementById("reset").addEventListener("click", function(event){
 		event.returnValue = false;
+		if (reqLock) return;
+		reqLock = true;
 		connec = new XMLHttpRequest();
 		connec.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				document.getElementById("kittyArea").innerHTML = this.responseText;
 				setEventA();
 			}
+			reqLock = false;
 		};
 		connec.open('GET','select_kitten.php', true);
 		connec.send();
@@ -14,12 +18,15 @@ function setEventA() {
 	[].slice.call(document.getElementsByClassName("kitty")).forEach(function(elmt) {
 		elmt.addEventListener("click", function(event){
 			event.returnValue = false;
+			if (reqLock) return;
+			reqLock = true;
 			connec = new XMLHttpRequest();
 			connec.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
 					document.getElementById("kittyArea").innerHTML = this.responseText;
 					setEventA();
 				}
+				reqLock = false;
 			};
 			connec.open('GET','selected.php?kitten='+this.getAttribute('data'), true);
 			connec.send();
